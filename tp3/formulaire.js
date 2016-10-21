d988043c0c33b66d236ea8a417dd00ab9dc32090
batache <batache.cylia@gmail.com>
2016-10-21 11:38:57 +0200

    Create formulaire.js

---
+++ tp3/formulaire.js
@@ -0,0 +1,17 @@
+function validation(){
+	var nom= document.getElementById("nom").value;
+	
+	
+	var prenom=document.getElementById("prenom").value;
+	var date=document.getElementById("date").value;
+	var adress=document.getElementById("adress").value;
+	var email=document.getElementById("mail").value;
+	
+	
+	if(nom == '' || prenom=="" || date=="" || adress=="" || email=="") 
+	  document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
+    else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adress.length>=5)&& (email.length>=5))
+		 document.getElementById("resultat").innerHTML = "Bienvenue jony "+prenom;
+		else
+			 document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
+}
