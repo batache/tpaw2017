3de3ab44c92fdec9b660307bc4247d9180d2b8eb
batache <batache.cylia@gmail.com>
2016-10-22 22:55:54 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -7,16 +7,16 @@ function validation(){
 	
 	var prenom=document.getElementById("prenom").value;
 	var date=document.getElementById("date").value;
-	var adress=document.getElementById("adr").value;
+	var adresse=document.getElementById("adr").value;
 	var email=document.getElementById("mail").value;
 	
 	
-	if(nom == '' || prenom=="" || date=="" || adress=="" || email=="") {
+	if(nom == '' || prenom=="" || date=="" || adresse=="" || email=="") {
 		$(".modal-body").html("La saisie des champs est obligatoire")
 	  	$('.modal').modal('show');
 	}
-    	else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adress.length>=5)&& (email.length>=5)){	    
-	    	$(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adress+'&zoom=13&size=300x300&maptype=roadmap&markers='+adress+'"/>');
+    	else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){	    
+	    	$(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&zoom=13&size=300x300&maptype=roadmap&markers='+adresse+'"/>');
             $('.modal').modal('show');
     	}
 	else{
