bad139cd2b020b1c3c4fe017ca47913a156738ac
batache <batache.cylia@gmail.com>
2016-10-23 21:39:01 +0200

    Create formulaire.js

---
+++ tp5/formulaire.js
@@ -0,0 +1,23 @@
+Validation(){
+
+
+    var nom= document.getElementById("nom").value;   
+    var prenom=document.getElementById("prenom").value;
+    var date=document.getElementById("date").value;
+    var adresse=document.getElementById("adresse").value;
+    var email=document.getElementById("mail").value;
+
+   if(nom == '' || prenom=="" || date=="" || adresse=="" || email=="") {
+        $(".modal-body").html("La saisie des champs est obligatoire");
+          $('.modal').modal('show');
+    }
+       else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){        
+              $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&zoom=13&size=300x300&maptype=roadmap&markers='+adresse+'"/>');
+            $('.modal').modal('show');
+       }
+    else{
+        $(".modal-body").html("veuillez remplir tous les champs svp!");
+        $('.modal').modal('show');
+    }
+
+}
