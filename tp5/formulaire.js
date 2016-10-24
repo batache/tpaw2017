1de51f6a184393ca259e8df6942dfb0b7c30782e
batache <batache.cylia@gmail.com>
2016-10-24 23:30:40 +0200

    Update formulaire.js

--- tp5/formulaire.js
+++ tp5/formulaire.js
@@ -3,6 +3,7 @@ function Validation(){
     
     
     var nom= document.getElementById("nom").value;    
+    
     var prenom=document.getElementById("prenom").value;
     var date=document.getElementById("date").value;
     var adresse=document.getElementById("adresse").value;
