a5b28229d7acb94f8804a7e34cb07ec2bb046842
batache <batache.cylia@gmail.com>
2016-10-24 23:21:21 +0200

    Update formulaire.js

--- tp5/formulaire.js
+++ tp5/formulaire.js
@@ -9,41 +9,16 @@ function Validation(){
     var email=document.getElementById("email").value;
     
     
-    if(nom =="" || prenom=="" || date=="" || adresse=="" || email=="") {
-          $(".modal-body").html("La saisie des champs est obligatoire")
-          $('.modal').modal('show');
-    }
-       else if ((nom.length>=5)&&(prenom.length>=5)&&(date.length>=5)&&(adresse.length>=5)&&(email.length>=5)){        
-           $(".modal-body").html('function validation(){
-    
-    
-    
-    var nom= document.getElementById("nom").value;
-    
-    
-    var prenom=document.getElementById("prenom").value;
-    var date=document.getElementById("date").value;
-    var adress=document.getElementById("adr").value;
-    var email=document.getElementById("mail").value;
-    
-    
-    if(nom == '' || prenom=="" || date=="" || adress=="" || email=="") {
+    if(nom == '' || prenom=="" || date=="" || adresse=="" || email=="") {
         $(".modal-body").html("La saisie des champs est obligatoire")
           $('.modal').modal('show');
     }
-       else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adress.length>=5)&& (email.length>=5)){        
-            $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adress+'&zoom=13&size=300x300&maptype=roadmap&markers='+adress+'"/>');
+       else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){        
+            $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&zoom=13&size=300x300&maptype=roadmap&markers='+adresse+'"/>');
             $('.modal').modal('show');
        }
     else{
         $(".modal-body").html("veuillez remplir tous les champs svp!")
         $('.modal').modal('show');
-    }
-}');
-           $('.modal').modal('show');
-       }
-    else{
-         $(".modal-body").html("veuillez remplir tous les champs svp!")
-         $('.modal').modal('show');
     }
 }
