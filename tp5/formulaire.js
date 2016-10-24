0f8e09cb7d3129bc0e0618736b1807b7cccf8055
batache <batache.cylia@gmail.com>
2016-10-25 00:35:13 +0200

    Update formulaire.js

--- tp5/formulaire.js
+++ tp5/formulaire.js
@@ -7,12 +7,12 @@ function Validation(){
     var email=document.getElementById("email").value;
     
     
-    if(nom == '' || prenom=="" || date=="" || adresse=="" || email=="") {
+    if(nom =="" || prenom=="" || date=="" || adresse=="" || email=="") {
             $(".modal-body").html("La saisie des champs est obligatoire")
             $('.modal').modal('show');
     }
        else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){        
-            $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&zoom=13&size=300x300&maptype=roadmap&markers='+adresse+'"/>');
+            $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&size=640x400&&markers='+adresse+'"/>');
             $('.modal').modal('show');
        }
     else{
