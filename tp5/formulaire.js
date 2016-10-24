4f4a67a205134472df687272bac5b81cbb518a73
batache <batache.cylia@gmail.com>
2016-10-24 23:59:15 +0200

    Update formulaire.js

--- tp5/formulaire.js
+++ tp5/formulaire.js
@@ -1,9 +1,7 @@
-function Validation(){
+function Validation(){ 
     
     
-    
-    var nom= document.getElementById("nom").value;
-    
+    var nom= document.getElementById("nom").value;    
     var prenom=document.getElementById("prenom").value;
     var date=document.getElementById("date").value;
     var adresse=document.getElementById("adresse").value;
@@ -11,15 +9,15 @@ function Validation(){
     
     
     if(nom == '' || prenom=="" || date=="" || adresse=="" || email=="") {
-        $(".modal-body").html("La saisie des champs est obligatoire")
-          $('.modal').modal('show');
+            $(".modal-body").html("La saisie des champs est obligatoire")
+            $('.modal').modal('show');
     }
        else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){        
             $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&zoom=13&size=300x300&maptype=roadmap&markers='+adresse+'"/>');
             $('.modal').modal('show');
        }
     else{
-        $(".modal-body").html("veuillez remplir tous les champs svp!")
-        $('.modal').modal('show');
+          $(".modal-body").html("veuillez remplir tous les champs svp!")
+          $('.modal').modal('show');
     }
 }
