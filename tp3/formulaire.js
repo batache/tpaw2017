bbf6d4f2b73e7967cfc3e68581de51053cfab81b
batache <batache.cylia@gmail.com>
2016-10-25 17:38:03 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -13,7 +13,7 @@ function Validation(){
                $('.modal').modal('show');
     }
        else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){        
-              $(".modal-body").html("BRAVO")
+               $(".modal-body").html('<img src="http://jqueryui.com/jquery-wp-content/themes/jquery/content/books/jquery-ui-in-action.jpg"/>')
                $('.modal').modal('show');
        }
     else{
