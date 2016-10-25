11f6c88c333b1fb5cc7b6ee27fa4c111dd38803f
batache <batache.cylia@gmail.com>
2016-10-25 18:07:59 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -13,7 +13,7 @@ function Validation(){
                $('.modal').modal('show');
     }
        else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(adresse.length>=5)&& (email.length>=5)){        
-              $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adresse+'&zoom=13&size=300x300&maptype=roadmap&markers='+adresse+'"/>')
+              $(".modal-body").html('<img src="https://www.youtube.com/embed/VI04yNch1hU;autoplay=1"/>')
                $('.modal').modal('show');
        }
     else{
