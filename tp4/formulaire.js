ca0d5b126c0909a8643852dc9200c3f60f23ce63
batache <batache.cylia@gmail.com>
2016-10-27 12:21:56 +0200

    Update formulaire.js

--- tp4/formulaire.js
+++ tp4/formulaire.js
@@ -13,7 +13,7 @@ function Validation(){
                $('.modal').modal('show');
     }
        else if ((nom.length>=5)&&(prenom.length>=5)&& (date.length>=5)&&(email.length>=5)){        
-              document.getElementById("resultat").innerHTML="bravo, successful or positive action."
+               document.getElementById("container").innerHTML = "Bienvenue jony "
        }
     else{
                $(".modal-body").html("veuillez remplir tous les champs svp!")
