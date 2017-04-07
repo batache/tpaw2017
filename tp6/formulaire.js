084913b8b526c137514397df701f1211fec6474d
batache <batache.cylia@gmail.com>
2017-04-07 15:01:45 +0200

    Update formulaire.js

--- tp6/formulaire.js
+++ tp6/formulaire.js
@@ -3,9 +3,12 @@
 function validation(){
 	
  
-            $(".modal-body").html('<video src="http://193.248.50.217:8280/viamotion2/live/eds/TF1/HLS/TF1.m3u8"/>');
-				  
-		$('.modal').modal('show');
+            //$(".modal-body").html('<video src="http://193.248.50.217:8280/viamotion2/live/eds/TF1/HLS/TF1.m3u8"/>');  
+	   // $('.modal').modal('show');
 	
+	
+            $(".modal-body").html("La saisie des champs est obligatoire")
+            $('.modal').modal('show');
+  
 }
 		
