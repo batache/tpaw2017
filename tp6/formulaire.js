218913d1ee13e139b7e3afc25a8d6703026b4f63
batache <batache.cylia@gmail.com>
2017-04-07 15:00:17 +0200

    Update formulaire.js

--- tp6/formulaire.js
+++ tp6/formulaire.js
@@ -6,4 +6,6 @@ function validation(){
             $(".modal-body").html('<video src="http://193.248.50.217:8280/viamotion2/live/eds/TF1/HLS/TF1.m3u8"/>');
 				  
 		$('.modal').modal('show');
+	
+}
 		
